10aed8725bc5bc50e04c5b9fa5eb467bf5759c4c
Melvin Tang <mxcodeyard@gmail.com>
2020-10-03 18:10:54 +0800

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 // modules
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const login = require('./routes/login');
 const register = require('./routes/register');
 
+// database setup
+mongoose
+  .connect('mongodb://localhost:27017/db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.error(err));
+
 // app setup
 app.use(express.json());
-app.use('/login', login);
-app.use('/register', register);
 
 // app routes
 app.get('/', (req, res) => res.send('Root route'));
+app.use('/login', login);
+app.use('/register', register);
 
 // port
 const PORT = 3000;
