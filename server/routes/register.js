de4e7b1142eac19274611c34413bce67c1020949
Melvin Tang <mxcodeyard@gmail.com>
2020-10-03 19:13:49 +0800

    Register route linked to mongoDB

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/', (req, res, next) => {
-  res.send('Register route');
+// user registration route
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+  user.save(function (err) {
+    if (err) return res.status(400).send({ error: 'Registration failed.' });
+
+    return res.json('Registration success.');
+  });
 });
 
 module.exports = router;
