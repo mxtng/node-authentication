f2d6f6efff7425c4929c28fc0622e7a4ca21610f
Melvin Tang <mxcodeyard@gmail.com>
2020-10-03 18:33:00 +0800

    Typo fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const userModel = new Schema({
   email: { type: String, lowercase: true, unique: true, required: true },
   password: { type: String, required: true },
 });
 
-module.exports = mongoose.Model('User', userModel);
+module.exports = mongoose.model('User', userModel);
