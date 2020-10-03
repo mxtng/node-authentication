f4551f39a89f8d7f92993974faf974c82b68e06a
Melvin Tang <mxcodeyard@gmail.com>
2020-10-03 20:02:53 +0800

    User schema with bcrypt added

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userModel = new Schema({
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
   email: { type: String, lowercase: true, unique: true, required: true },
   password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('User', userModel);
+userSchema.pre('save', function (next) {
+  let { password } = this;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(password, salt, function (err, hash) {
+      if (err) return next(err);
+      password = hash;
+      next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', userSchema);
