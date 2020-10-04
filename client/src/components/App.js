050b5f843ae2ae820f0241c0b5dbd2c0ea6acab5
Melvin Tang <mxcodeyard@gmail.com>
2020-10-04 16:04:32 +0800

    Initial redux setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import reducer from '../reducers';
+import thunk from 'redux-thunk';
 
 import Navbar from './navbar/Navbar';
 import Root from './pages/Root';
@@ -7,16 +11,22 @@ import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
+// redux store setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 const App = () => {
   return (
-    <div className="container">
-      <BrowserRouter>
-        <Navbar />
-        <Route path='/' exact component={Root} />
-        <Route path='/dashboard' component={Dashboard} />
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-      </BrowserRouter>
+    <div className='container'>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Route path='/' exact component={Root} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 };
