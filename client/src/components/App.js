9ebe3d7823e821803d0c0773cfbb54da6d3c1c16
Melvin Tang <mxcodeyard@gmail.com>
2020-10-04 14:12:29 +0800

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Navbar from './navbar/Navbar';
+import Root from './pages/Root';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Route path='/' exact component={Root} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
